4bca4cca9c26ce4f1279d0fd59ad59e5e6da72bf
Dubrovschii <dubrovschii@gmail.com>
2025-02-19 12:48:23 +0200

    Change apistore

--- src/stores/apiStore.js
+++ src/stores/apiStore.js
@@ -8,8 +8,8 @@ const { notify } = useNotification();
 
 export const useApiStore = defineStore("apiStore", {
     state: () => ({
-        // baseLink: "http://localhost:5173/",
-        baseLink: "https://ai-project-neon.vercel.app",
+        baseLink: "http://localhost:5173/",
+        // baseLink: "https://ai-project-neon.vercel.app",
         authorName: "",
         message: "",
         currentLang: localStorage.getItem("currentLang") || "en",
